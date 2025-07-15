090ac23b4420f694d55d996d0cd3bb392561e8d2
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-15 09:05:57 +0530

    mock-data-integrated-in-/users

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json")
 
 
 const app = express();
@@ -6,16 +7,9 @@ const app = express();
 const PORT = 8000;
 
 // Routes
-app.get("/",(req, res)=>{
-    return res.send("hello from home page");
+app.get("/users", (req, res) => {
+    return res.json(users);
 })
-
-app.get("/about", (req, res) => {
-  return res.send("hello from about page");
-});
-app.get("/contact", (req, res) => {
-  return res.send("hello from contact page" + " Hi " + req.query.name + "your age is " + req.query.age);
-});
 
 app.listen(PORT, () => {
   console.log(`Server started from ${PORT} `);
