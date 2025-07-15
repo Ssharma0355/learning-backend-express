f63b33c0f467adaf7a177451d550fe43b6529a34
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-15 10:37:25 +0530

    sending dynamic apis for mobile app and web apllication

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ app.use(cors());
 
 const PORT = 8000;
 
-// Routes
-app.get("/users", (req, res) => {
+// Routes for mobile application we sending json
+app.get("/api/users", (req, res) => {
     return res.json(users);
+})
+// for web browser we are sending HTML DOC SSR
+app.get("/users",(req, res)=>{
+    const html = `
+    <ul>
+    ${users.map(user =>(`<li>${user.first_name}</li>`)).join("")}
+    </ul>
+    `
+    return res.send(html)
 })
 
 app.listen(PORT, () => {
