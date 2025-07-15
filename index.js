b732ef630f58611312c955c2666aa2782024df8f
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-15 10:56:32 +0530

    Grouping of route

--- index.js
+++ index.js
@@ -22,13 +22,25 @@ app.get("/users",(req, res)=>{
     return res.send(html)
 })
 // Dynamic path for getting a specific user from DB
-
-app.get("/api/users/:id",(req,res)=>{
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-
-})
+  app
+    .route("/api/users/:id")
+    .get((req, res) => {
+      const id = Number(req.params.id);
+      const user = users.find((user) => user.id === id);
+      return res.json(user);
+    })
+    .post((req, res) => {
+      return res.json({ status: "pending" });
+    })
+    .put((req, res) => {
+      return res.json({ status: "pending" });
+    })
+    .patch((req, res) => {
+      res.json({ status: "pending" });
+    })
+    .delete((req, res) => {
+      res.json({ status: "pending" });
+    });
 
 app.listen(PORT, () => {
   console.log(`Server started from ${PORT} `);
